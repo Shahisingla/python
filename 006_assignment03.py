21b80809220b3c770838b55a773b09eff8883a2f
Shahisingla <95943275+Shahisingla@users.noreply.github.com>
2021-12-12 21:59:17 -0500

    Delete 006_assignment03.py

--- 006_assignment03.py
+++
@@ -1,7 +0,0 @@
-hh=int(input("give me the start time hour: "))
-mm=int(input("give me the start time minute: "))
-dd=int(input("give me the duration in the minutes: "))
-z=hh*60+mm+dd
-hh=z//60
-mm=z-hh*60
-print("the finish time is=",hh,":",mm)
