4ab93ebad1849e68fea372478e9a394dd62e8bcc
Shahisingla <95943275+Shahisingla@users.noreply.github.com>
2021-12-19 15:32:44 -0500

    Add files via upload

---
+++ 021_nested_for.py
@@ -0,0 +1,8 @@
+n=int(input("enter the value of n:"))      
+for i in range(1,n+1):
+    print("\n")
+    for j in range(1,n+1):
+        print(i*j,end="\t")
+        
+print("\n\nthe end.")
+        
